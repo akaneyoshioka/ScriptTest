83d167d82bf5a81176457ed2da75965bd01551c6
吉岡茜 <sode_take@icloud.com>
2018-08-24 23:25:27 +0900

    ScriptTestプロジェクト初めてのコミット
    C#スクリプトのサンプルです。

---
+++ Assets/Test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss{
+	private int hp=100;
+	private int power=25;
+	private int mp=53;
+
+	public void Attack(){
+		Debug.Log (this.power + "のダメージを与えた");
+	}
+	public void Defence(int damage){
+		Debug.Log (damage + "のダメージを受けた");
+	}
+	public void Magic(int a){
+		int b = mp - a;
+		if (b > 53) {
+			Debug.Log ("MPが足りないため魔法が使えない。");
+		} else {
+			Debug.Log ("魔法攻撃をした。残りMPは" + b);
+		}
+	}
+			
+}
+
+public class Test : MonoBehaviour {
+
+	void Start(){
+		Boss lastboss = new Boss ();
+		lastboss.Attack ();
+		lastboss.Defence (3);
+
+		Boss hit = new Boss ();
+		hit.Magic (5);
+
+		int[] array = { 1, 2, 3, 4, 5 };
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log (array [i]);
+		}
+	}
+	void Update(){
+	}
+
+}
